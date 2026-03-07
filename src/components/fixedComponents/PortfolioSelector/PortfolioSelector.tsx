24df1932a90ded5e40ae96c2d2394a33643bce64
Tazz Akber <90526357+butteredtoasts@users.noreply.github.com>
2026-03-06 23:40:08 -0500

    - Changed font and tab icon - Completed PhotoMain - Added some new photos for PhotoMain

--- src/components/fixedComponents/PortfolioSelector/PortfolioSelector.tsx
+++ src/components/fixedComponents/PortfolioSelector/PortfolioSelector.tsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
 
-import "./PortfolioSelector.css"
+import "./PortfolioSelector.css";
 import FadeUp from "../../reusable/FadeUp/FadeUp";
 
 function PortfolioSelector() {
-    return(
-        <>
-            <FadeUp delay={300}>
-            <div className="portfolioButtons">
-                <Link to="/portfolio/dev" className="portfolioButtonSingle">Development</Link>
-                <Link to="/portfolio/photography" className="portfolioButtonSingle">Photography</Link>
-                <Link to="/portfolio/music" className="portfolioButtonSingle">Music</Link>
-                <Link to="/portfolio/content" className="portfolioButtonSingle">Content</Link>
-            </div>
-            </FadeUp>
-        </>
-    )
+  return (
+    <>
+      <FadeUp delay={300}>
+        <div className="portfolioButtons">
+          <Link to="/portfolio/dev" className="portfolioButtonSingle">
+            Development
+          </Link>
+          <Link to="/portfolio/photography" className="portfolioButtonSingle">
+            Photography
+          </Link>
+          <Link to="/portfolio/music" className="portfolioButtonSingle">
+            Music
+          </Link>
+          <Link to="/portfolio/content" className="portfolioButtonSingle">
+            Content
+          </Link>
+        </div>
+      </FadeUp>
+    </>
+  );
 }
 
 export default PortfolioSelector;
--- src/components/pages/PhotoMain/PhotoMain.tsx
+++ src/components/pages/PhotoMain/PhotoMain.tsx
@@ -2,27 +2,54 @@ import PhotoLink from "../../reusable/PhotoLink/PhotoLink";
 import FadeUp from "../../reusable/FadeUp/FadeUp";
 
 import "./PhotoMain.css";
+import aboutPhotosText from "./aboutPhotos.txt?raw";
 
 import MountainBoy from "../../../assets/Photography/Portraits/MountainBoy.jpg";
 import BabyShower from "../../../assets/Photography/Events/BabyShower.jpg";
 
+import photoshoot1 from "../../../assets/Self/Photoshoot1.jpeg";
+import photoshoot2 from "../../../assets/Self/Photoshoot2.jpeg";
+
 function PhotoMain() {
-    const portraitsLink="/portfolio/photography/portraits";
-    const eventsLink="/portfolio/photography/events";
+  const portraitsLink = "/portfolio/photography/portraits";
+  const eventsLink = "/portfolio/photography/events";
 
-    const portraitsLinkText="Portraits";
-    const eventsLinkText="Events";
+  const portraitsLinkText = "Portraits";
+  const eventsLinkText = "Events";
 
-    return (
-        <>
-            <FadeUp delay={500}>
-            <div className="photoTypeOptionsBox">
-                <PhotoLink linkTo={portraitsLink} imageUsed={MountainBoy} linkText={portraitsLinkText} />
-                <PhotoLink linkTo={eventsLink} imageUsed={BabyShower} linkText={eventsLinkText} />
-            </div>
-            </FadeUp>
-        </>
-    )
+  return (
+    <>
+      <FadeUp delay={500}>
+        <div className="photoTypeOptionsBox">
+          <PhotoLink
+            linkTo={portraitsLink}
+            imageUsed={MountainBoy}
+            linkText={portraitsLinkText}
+          />
+          <PhotoLink
+            linkTo={eventsLink}
+            imageUsed={BabyShower}
+            linkText={eventsLinkText}
+          />
+        </div>
+      </FadeUp>
+      <FadeUp delay={600}>
+        <div className="photoAboutFlexBox">
+          <div className="photographyAboutBox">
+            <h2 className="photoAboutHeader">About My Photography</h2>
+            <p
+              className="photoAboutText"
+              dangerouslySetInnerHTML={{ __html: aboutPhotosText }}
+            ></p>
+          </div>
+          <div className="cameraPhotos">
+            <img src={photoshoot1} />
+            <img src={photoshoot2} />
+          </div>
+        </div>
+      </FadeUp>
+    </>
+  );
 }
 
 export default PhotoMain;
